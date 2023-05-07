6b3cb5787c8fd0d40923c7b0b006d0543adaa0b9
Felix Stricker <kontakt@felixstricker.ch>
2023-05-07 13:35:02 +0200

    add mode toggle

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log(document.querySelector('.textfield input[type=date]'));
-
+    // datefield helper
     const dateElements = document.querySelectorAll(
         '.textfield input[type=date]'
     );
@@ -12,4 +11,24 @@ document.addEventListener('DOMContentLoaded', function () {
         item.onblur = () =>
             item.value === '' ? (item.type = 'text') : (item.type = 'date');
     });
+
+    // mode toggle
+    const modeToggleElement = document.querySelector('.js-mode-toggle');
+    const bodyElement = document.querySelector('body');
+    let mode = localStorage.getItem('mode');
+    if (mode === null) {
+        mode = 'lightmode';
+        localStorage.setItem('mode', mode);
+    }
+    bodyElement.classList.add(mode);
+
+    if (modeToggleElement) {
+        modeToggleElement.checked = mode === 'darkmode';
+        modeToggleElement.onchange = () => {
+            bodyElement.classList.remove(mode);
+            mode = mode === 'lightmode' ? 'darkmode' : 'lightmode';
+            localStorage.setItem('mode', mode);
+            bodyElement.classList.add(mode);
+        };
+    }
 });

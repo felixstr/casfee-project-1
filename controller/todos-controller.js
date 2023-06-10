6df39310d351090e91d12cd6a2c865700f6a65bb
Felix Stricker <kontakt@felixstricker.ch>
2023-06-10 17:51:20 +0200

    lint fix

--- controller/todos-controller.js
+++ controller/todos-controller.js
@@ -1,12 +1,12 @@
 import { todosStore } from '../services/todos-store.js';
 
 export class TodosController {
-    getTodos = async (req, res) => {
+    async getTodos(req, res) {
         res.json(await todosStore.all());
-    };
+    }
 
-    addTodo = async (req, res) => {
-        console.log('req.body', req.body);
+    async addTodo(req, res) {
+        // console.log('req.body', req.body);
         res.json(
             await todosStore.add(
                 req.body.title,
@@ -17,10 +17,10 @@ export class TodosController {
                 req.body.createdate
             )
         );
-    };
+    }
 
-    updateTodo = async (req, res) => {
-        console.log('updateTodo', req.body);
+    async updateTodo(req, res) {
+        // console.log('updateTodo', req.body);
         res.json(
             await todosStore.update(
                 req.params.id,
@@ -32,15 +32,15 @@ export class TodosController {
                 req.body.createdate
             )
         );
-    };
+    }
 
-    deleteTodo = async (req, res) => {
+    async deleteTodo(req, res) {
         res.json(await todosStore.delete(req.params.id)); // TODO should return 402 if not ok
-    };
+    }
 
-    getTodo = async (req, res) => {
+    async getTodo(req, res) {
         res.json(await todosStore.get(req.params.id));
-    };
+    }
 }
 
 export const todosController = new TodosController();
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-// load app with current config
-const app = (await import('./app.js')).app;
+import express from 'express';
+import bodyParser from 'body-parser';
+import { apiRoutes } from './routes/api-routes.js';
+
+export const app = express();
+
+// middlewares
+function notFound(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(404, '404 - Page not found!');
+}
+
+app.use(express.static('source/public'));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    setTimeout(next, 1000);
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html', { root: '/public/' });
+});
+
+app.use('/api', apiRoutes);
+app.use(notFound);
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3001;
+const port = 3001;
 app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server running at http://${hostname}:${port}/`);
 });

5e5f3788fcb768a9cb603a17829c6ba6d857f82a
Felix Stricker <kontakt@felixstricker.ch>
2023-06-16 12:53:55 +0200

    simplify index.js

--- index.js
+++ index.js
@@ -2,27 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { apiRoutes } from './source/server/routes/api-routes.js';
 
-export const app = express();
-
-// middlewares
-function notFound(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(404, '404 - Page not found!');
-}
+const app = express();
 
 app.use(express.static('source/client'));
 app.use(bodyParser.json());
-/*
-app.use(function (req, res, next) {
-    setTimeout(next, 1000);
-});
-*/
-app.get('/', function (req, res) {
-    res.sendFile('/index.html', { root: '/client/' });
-});
-
 app.use('/api', apiRoutes);
-app.use(notFound);
+app.use((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(404).send('404 - Page not found!!');
+});
 
 const hostname = '127.0.0.1';
 const port = 3001;

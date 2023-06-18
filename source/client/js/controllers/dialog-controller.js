986d5cf250b15238d25270d351a0f936742368aa
Felix Stricker <kontakt@felixstricker.ch>
2023-06-18 12:31:52 +0200

    generate dialgo html with template string

--- source/client/js/controllers/dialog-controller.js
+++ source/client/js/controllers/dialog-controller.js
@@ -1,18 +1,20 @@
-const SELECTOR_DIALOG = '.js-dialog';
-const SELECTOR_BUTTON_CANCEL = '.js-button-cancel';
-const SELECTOR_FORM = '.js-form';
+const SELECTOR_DIALOG = 'js-dialog';
+const SELECTOR_BUTTON_CANCEL = 'js-button-cancel';
+const SELECTOR_FORM = 'js-form';
 export default class DialogController {
     constructor() {
-        this.dialogElement = document.querySelector(SELECTOR_DIALOG);
-        this.buttonCancelElement = document.querySelector(SELECTOR_BUTTON_CANCEL);
-        this.formElement = document.querySelector(SELECTOR_FORM);
+        this.render();
+        this.dialogElement = document.querySelector('.' + SELECTOR_DIALOG);
+        this.buttonCancelElement = this.dialogElement.querySelector('.' + SELECTOR_BUTTON_CANCEL);
+        this.formElement = this.dialogElement.querySelector('.' + SELECTOR_FORM);
+        this.titleElement = this.dialogElement.querySelector('h2');
+        this.changeSpecialInputTypeWhileFocus();
 
         this.titleFieldElement = this.formElement.querySelector('[name="title"]');
         this.descriptionFieldElement = this.formElement.querySelector('[name="description"]');
         this.duedateFieldElement = this.formElement.querySelector('[name="duedate"]');
         this.priorityFieldElement = this.formElement.querySelector('[name="priority"]');
         this.idFieldElement = this.formElement.querySelector('[name="id"]');
-        this.titleElement = this.formElement.querySelector('h2');
 
         this.onSubmit = () => console.error('onSubmit() function is not implemented');
 
@@ -72,5 +74,90 @@ export default class DialogController {
             event.target.classList.remove('close');
             event.target.removeEventListener('animationend', closeDialogAfter);
         }
+    }
+
+    render() {
+        document.body.insertAdjacentHTML(
+            'beforeend',
+            `<dialog class="modal ${SELECTOR_DIALOG}">
+                <div class="l-wrapper">
+                    <h2>Edit Todo</h2>
+                    <form class="${SELECTOR_FORM}">
+                        <input type="hidden" name="id" />
+                        <div class="l-form">
+                            <div class="textfield">
+                                <input
+                                    id="title"
+                                    type="text"
+                                    name="title"
+                                    value=""
+                                    placeholder="hidden"
+                                    required
+                                />
+                                <label for="title">Title</label>
+                            </div>
+
+                            <div class="textfield">
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    placeholder="hidden"
+                                ></textarea>
+                                <label for="description">Description</label>
+                            </div>
+
+                            <div class="l-form__row">
+                                <div class="textfield">
+                                    <input
+                                        id="duedate"
+                                        type="date"
+                                        name="duedate"
+                                        value=""
+                                        placeholder="hidden"
+                                    />
+                                    <label for="duedate">Due date</label>
+                                </div>
+
+                                <div class="textfield">
+                                    <input
+                                        id="prio"
+                                        type="number"
+                                        name="priority"
+                                        value=""
+                                        placeholder="hidden"
+                                        min="1"
+                                        max="5"
+                                    />
+                                    <label for="prio">Priority</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="l-button-row">
+                            <button type="button" class="button ${SELECTOR_BUTTON_CANCEL}">Cancel</button>
+                            <button type="submit" class="button button--primary">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </dialog>`
+        );
+    }
+
+    changeSpecialInputTypeWhileFocus() {
+        // to get the floating labels work with date and number fields, the type must be changed to text when not focused (date and number fields do not have a placolder, which is needed go get the floating label work with css)
+        this.formElement
+            .querySelectorAll('.textfield input[type=date], .textfield input[type=number]')
+            .forEach((item) => {
+                const originalType = item.type;
+                item.type = 'text';
+
+                const fnResetType = () => {
+                    item.value === '' ? (item.type = 'text') : (item.type = originalType);
+                };
+
+                item.addEventListener('focus', () => (item.type = originalType));
+                item.addEventListener('blur', fnResetType);
+                item.addEventListener('resetType', fnResetType);
+            });
     }
 }
--- source/client/js/controllers/ui-controller.js
+++ source/client/js/controllers/ui-controller.js
@@ -1,31 +1,12 @@
 import { settingsStorage } from '../services/settings-storage.js';
 class UiController {
     constructor() {
-        this.changeSpecialInputTypeWhileFocus();
         this.initModeToggle();
-    }
-
-    changeSpecialInputTypeWhileFocus() {
-        // to get the floating labels work with date and number fields, the type must be changed to text when not focused (date and number fields do not have a placolder, which is needed go get the floating label work with css)
-        document
-            .querySelectorAll('.textfield input[type=date], .textfield input[type=number]')
-            .forEach((item) => {
-                const originalType = item.type;
-                item.type = 'text';
-
-                const fnResetType = () => {
-                    item.value === '' ? (item.type = 'text') : (item.type = originalType);
-                };
-
-                item.addEventListener('focus', () => (item.type = originalType));
-                item.addEventListener('blur', fnResetType);
-                item.addEventListener('resetType', fnResetType);
-            });
     }
 
     initModeToggle() {
         const modeToggleElement = document.querySelector('.js-mode-toggle');
-        const bodyElement = document.querySelector('body');
+        const bodyElement = document.body;
         let mode = settingsStorage.getItem('colormode') || 'lightmode';
         bodyElement.classList.add(mode);
 
